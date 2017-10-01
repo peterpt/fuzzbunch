e56f241357133e6106487edb73edc6960fe34653
peterpt <pedroamaropt@portugalmail.pt>
2017-10-01 14:53:21 +0100

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,24 @@
+cd dependencies
+
+msiexec /i python-2.6.6.msi
+
+pywin32-221.exe
+jre-6-windows-i586.exe
+
+reg ADD reg.reg
+
+mkdir c:\fuzzbunch
+
+mkdir d:\Logs
+
+mkdir d:\Logs\PROJECT_NAME
+
+mkdir d:\Logs\First
+
+cd ..
+
+xcopy * c:\fuzzbunch /e /i
+cd dependencies
+
+xcopy *.lnk "%USERPROFILE\Desktop"
+echo DONE
